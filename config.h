f6a4b0269e7df4b8ba296b23182bdce5130ae5d7
code399 <code399@163.com>
2023-07-02 16:29:38 +0800

    Add cpu rem vol...

--- config.h
+++ config.h
@@ -64,6 +64,12 @@ static const char unknown_str[] = "n/a";
  * wifi_perc           WiFi signal in percent          interface name (wlan0)
  */
 static const struct arg args[] = {
-	/* function format          argument */
-	{ datetime, "%s",           "%F %T" },
+	/* function 		format          argument */
+	/*{ run_command,	" %s |",	"uname -r | awk -F \"-\" '{ print $1 }'"},*/
+	/*{ disk_free,		" %s |",	"/" },*/
+	{ cpu_perc,		" %s%% |",	NULL},
+	{ ram_perc,		" %s%% |",	NULL},
+	{ run_command,		" %s |",	"amixer sget Master | awk -F \"[][]\" 'NF>1 { print $2 }'"},
+	/*{ run_command,	" %s |",	"amixer sget Capture | awk -F \"[][]\" '/Left:/ { print $2 }'"},*/
+	{ datetime,		"%s",		"%y-%m_%d %a" },
 };
